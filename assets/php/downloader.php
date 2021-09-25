e6a6f518e51c947ba4f6663106c65ebf7f4c43b8
NightTab <88324388+NightTabGit@users.noreply.github.com>
2021-09-25 14:23:48 +0100

    Delete assets directory

--- assets/php/downloader.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$hit_count = @file_get_contents('count.txt');
-$hit_count++;
-@file_put_contents('count.txt', $hit_count);
-
-header('https://github.com/Kopamed/Raven-bPLUS/raw/main/build/libs/%5B1.8.9%5D%20BetterKeystrokes%20V-1.1.0.jar'); // redirect to the real file to be downloaded
